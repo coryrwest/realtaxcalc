cef7656c21a5eeddb51404b3b44bd32c0e538521
Cory Westropp <cory.r.west@gmail.com>
2016-01-06 09:41:46 -0800

    Tests

---
+++ tests/FedTaxBracket.js
@@ -0,0 +1,100 @@
+var assert = require('chai').assert;
+import FedTaxBracket from "../src/common/FedTaxBracket";
+
+
+var bracket = '10%';
+describe('The '+bracket+' tax bracket', function(){
+  var income = 5463;
+  
+  var bracket = new FedTaxBracket(0, 9225, 0.10, 0);
+  
+  var tax = bracket.calculateBracketTax(income);
+  
+  it('should return the correct tax', function(){
+    assert.equal(tax, 546.30, 'Tax is not '+bracket+' ' + tax);
+  });
+});
+
+
+bracket = '15%';
+describe('The '+bracket+' tax bracket', function(){
+  var income = 10000;
+  
+  var bracket = new FedTaxBracket(9226, 37450, 0.15, 922.50);
+  
+  var tax = bracket.calculateBracketTax(income);
+  
+  it('should return the correct tax', function(){
+    assert.equal(tax, 1038.75, 'Tax is not '+bracket+' ' + tax);
+  });
+});
+
+
+bracket = '25%';
+describe('The '+bracket+' tax bracket', function(){
+  var income = 45600;
+  
+  var bracket = new FedTaxBracket(37451, 90750, 0.25, 5156.25);
+  
+  var tax = bracket.calculateBracketTax(income);
+  
+  it('should return the correct tax', function(){
+    assert.equal(tax, 7193.75, 'Tax is not '+bracket+' ' + tax);
+  });
+});
+
+
+bracket = '28%';
+describe('The '+bracket+' tax bracket', function(){
+  var income = 150900;
+  
+  var bracket = new FedTaxBracket(90751, 189300, 0.28, 18481.25);
+  
+  var tax = bracket.calculateBracketTax(income);
+  
+  it('should return the correct tax', function(){
+    assert.equal(tax, 35323.25, 'Tax is not '+bracket+' ' + tax);
+  });
+});
+
+
+bracket = '33%';
+describe('The '+bracket+' tax bracket', function(){
+  var income = 280900;
+  
+  var bracket = new FedTaxBracket(189301, 411500, 0.33, 46075.25);
+  
+  var tax = bracket.calculateBracketTax(income);
+  
+  it('should return the correct tax', function(){
+    assert.equal(tax, 76303.25, 'Tax is not '+bracket+' ' + tax);
+  });
+});
+
+
+bracket = '35%';
+describe('The '+bracket+' tax bracket', function(){
+  var income = 412000;
+  
+  var bracket = new FedTaxBracket(411501, 413200, 0.35, 119401.25);
+  
+  var tax = bracket.calculateBracketTax(income);
+  
+  it('should return the correct tax', function(){
+    assert.equal(tax, 119576.25, 'Tax is not '+bracket+' ' + tax);
+  });
+});
+
+
+bracket = '39.6%';
+describe('The '+bracket+' tax bracket', function(){
+  var income = 900000;
+  
+  var bracket = new FedTaxBracket(413201, 0, 0.396, 119996.25);
+  
+  var tax = bracket.calculateBracketTax(income);
+  
+  it('should return the correct tax', function(){
+    assert.equal(tax, 312769.05, 'Tax is not '+bracket+' ' + tax);
+  });
+});
